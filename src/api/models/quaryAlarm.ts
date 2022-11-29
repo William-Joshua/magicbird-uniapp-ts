50da0302d32ca58f5d538c0dd627c9896c44a1da
William Joshua <yourenlaila@hotmail.com>
2022-11-30 07:32:46 +0800

    feat: warning page

---
+++ src/api/models/quaryAlarm.ts
@@ -0,0 +1,37 @@
+export interface IDeviceAlarm {
+  alarmDate: Date;
+  siteName: string;
+  AlarmItem: Array<IAlarmItem>;
+}
+
+export interface IAlarmItem {
+  alarmTime: string;
+  deviceName: string; // 设备类型，1：视频监控类 2：水质曲线类 3：设备控制类
+  fieldName: string;
+  val: string;
+  alarmLevel: number;
+  alarmLevelName: string;
+  description: string;
+}
+export interface IAlarmData {
+  id: number;
+  fieldId: number;
+  placeId: number;
+  deviceId: number;
+  deviceName: string;
+  fieldName: string;
+  siteName: string;
+  alarmLevel: number;
+  alarmLevelName: string;
+  dealName: string;
+  dealTimes: number;
+  val: string;
+  userId: number;
+  userName: string;
+  isMsg: string;
+  isClear: string;
+  isDeal: string;
+  isWx: string;
+  acpTime: string;
+  alarmTime: string;
+}
---
+++ src/pages/alarm/components/alarmDetail.vue
@@ -0,0 +1,65 @@
+<template>
+  <view>
+    <view class="fixed">
+      <cu-custom :is-back="true" bgColor="bg-gradual-red">
+        <template v-slot:backText> 返回 </template>
+        <template v-slot:content> 警告详情 </template>
+      </cu-custom>
+      <view class="cu-list">
+        <view class="cu-item">
+          <form>
+            <view class="content">
+              <view>
+                <view class="cu-bar bg-white solid-bottom text-gray">
+                  <view class="action"> <text class="cuIcon-title text-orange"></text> 警告信息 </view>
+                </view>
+                <view class="cu-form-group text-black">
+                  <view class="title">警告编号</view>
+                  <input placeholder="警告编号" name="input" v-model="item.deviceId" />
+                </view>
+                <view class="cu-form-group text-black">
+                  <view class="title">设备名</view>
+                  <input placeholder="设备名" name="input" class="" v-model="item.deviceName" />
+                </view>
+                <view class="cu-form-group text-black">
+                  <view class="title">设备型号</view>
+                  <input placeholder="设备地址" name="input" v-model="item.deviceAddr" />
+                  <text class="cuIcon-locationfill text-orange"></text>
+                </view>
+                <view class="cu-form-group text-black">
+                  <view class="title">警告值</view>
+                  <input placeholder="警告值" name="input" v-model="item.deviceAddr" />
+                  <text class="cuIcon-locationfill text-orange"></text>
+                </view>
+                <view class="cu-form-group text-black">
+                  <view class="title">站点名称</view>
+                  <input placeholder="站点名称" name="input" readonly v-model="item.deviceTypeName" />
+                  <text class="cuIcon-info text-orange"></text>
+                </view>
+                <view class="cu-form-group text-black">
+                  <view class="title">警告等级</view>
+                  <input placeholder="警告等级" name="input" v-model="item.deviceCode" />
+                  <text class="cuIcon-barcode text-orange"></text>
+                </view>
+                <view class="cu-form-group text-black">
+                  <view class="title">警告等级</view>
+                  <input placeholder="警告等级" name="input" v-model="item.deviceCode" />
+                  <text class="cuIcon-barcode text-orange"></text>
+                </view>
+                <view class="cu-form-group text-black">
+                  <view class="title">警告等级</view>
+                  <input placeholder="警告等级" name="input" v-model="item.deviceCode" />
+                  <text class="cuIcon-barcode text-orange"></text>
+                </view>
+              </view>
+            </view>
+          </form>
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script></script>
+
+<style></style>
